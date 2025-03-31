45c8736de09de3f075cbb3d63cd3e067a51b0799
Aditya Tile <adityatile99@gmail.com>
2025-03-31 23:42:23 +0530

    create display tech icons component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+
+import InterviewCard from "@/components/InterviewCard";
 
 const Home = () => {
   return (
@@ -23,6 +26,25 @@ const Home = () => {
           height={400}
           className="max-sm:hidden"
         />
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+          {/* <p>There are no interviews available</p> */}
+        </div>
       </section>
     </>
   );
